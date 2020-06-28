313fde8423ee7b7ac0aa30d4d7ca8a3e30d685eb
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-06-28 21:18:00 +0200

    Add Crazy Hover

--- nanco-portfolio/src/pages/index.js
+++ nanco-portfolio/src/pages/index.js
@@ -3,28 +3,41 @@ import {Link} from "gatsby";
 
 import './home.css';
 
-const Home = () => (
-    <Fragment>
-        <div className="home">
-            <div className="home-content">
-                <h1>Morteza Jalilifar</h1>
-                <p>
-                    Hi, I'm Mori! Welcome to my page.
-                </p>
-                <p>
-                    This is a brief introduction of mine, my projects and skills, fasten your seatbelt!
-                </p>
-                <Link
-                    to='/about'
-                    activeClassName="active"
-                >
-                    About Me
-                </Link>
+const Home = () => {
+    const handleMouseOver = () => {
+        const homeBgEl = document.querySelector('.home-bg');
+        homeBgEl.classList.add('hover');
+    };
+
+    const handleMouseOut = () => {
+        const homeBgEl = document.querySelector('.home-bg');
+        homeBgEl.classList.remove('hover');
+    };
+
+    return (
+        <Fragment>
+            <div className="home">
+                <div className="home-content">
+                    <h1>Morteza Jalilifar</h1>
+                    <p>
+                        Hi, I'm Mori! Welcome to my page.
+                    </p>
+                    <p>
+                        This is a brief introduction of mine, my projects and skills, fasten your seatbelt!
+                    </p>
+                    <Link
+                        to='/about'
+                        onMouseOver={handleMouseOver}
+                        onMouseOut={handleMouseOut}
+                    >
+                        About Me
+                    </Link>
+                </div>
+                <div className="home-bg">
+                </div>
             </div>
-            <div className="home-bg">
-        </div>
-        </div>
-    </Fragment>
-);
+        </Fragment>
+    );
+};
 
 export default Home
