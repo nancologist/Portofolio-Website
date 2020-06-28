b3f72962990300f7e13231bb6714ea804bfab956
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-06-29 01:23:53 +0200

    update about me

--- nanco-portfolio/src/pages/about/index.js
+++ nanco-portfolio/src/pages/about/index.js
@@ -9,10 +9,10 @@ export default () => (
         <div className="about-content">
             <h2>Codes, bugs, errors, my second family.</h2>
             <p>
-                Since July 2019 I have changed my career from Physics to IT and started my apprenticeship at <a target="_blank" href="https://www.digitaspixelpark.com/">Digitas Pixelpark</a> backend team of the company.
-                It was not an overnight decision. My bachelor thesis had a small programming part with Matlab. It was a great experience for me, because more than coding I discovered that I love errors and bugs! The longer it takes to solve the bug, the more excited I am to solve it. Giving up is no option for me.
-                I'm also an all-around programmer, the strongly typed Java, the simple Python with those great communities and libraries, the controversial JS with beautiful Vue, and reactive React and the reigning PHP in WWW are all beautiful for me.
-                When I learn a new technology, I also learn and stick to the clean code, best practices and conventions in that language or framework. Even if I write a small code for my self I try to satisfy all those rules.
+                In July 2019 I have changed my career from Physics to IT and started my apprenticeship at <a target="_blank" href="https://www.digitaspixelpark.com/">Digitas Pixelpark</a>. It was not an overnight decision. My bachelor thesis had a small programming part with Matlab. It was a great experience for me, because more than coding I discovered that I love challenging errors and bugs! The longer it takes to solve them, the more excited I am to solve it. Giving up is no option for me.
+            </p>
+            <p>
+                In the last two years I have learned and tried dozens of languages and frameworks: Java, Python, PHP, JavaScript, Spring, Django, Vue, React, NodeJS, Express etc. to find the right one for me. Surprisingly I loved them all. They are all some kind of artworks to me, with different aspects and features.
             </p>
         </div>
     </div>
