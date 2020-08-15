96cf737441592740f39d85b0330e52dbab0a553d
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-15 17:03:36 +0200

    #Contact Form
    Add an input field

--- nanco-portfolio/src/pages/contact/index.js
+++ nanco-portfolio/src/pages/contact/index.js
@@ -1,8 +1,30 @@
 import React from "react";
-import MainLayout from "../../layouts/index";
+import {FormControl, InputLabel, Input} from '@material-ui/core';
+import './contact.css';
 
-export default () => (
-    <React.Fragment>
-        <p>You can contact Mori through... :</p>
-    </React.Fragment>
-);
+export default () => {
+    return (
+        <div className="page-container contact">
+            <h1 className="contact__title">Contact Me</h1>
+            <FormControl className="contact__form">
+                <div>
+                    <InputLabel
+                        htmlFor="my-input"
+                        classes={{
+                            root: 'contact__form__label',
+                            focused: 'contact__form__label--focused'
+                        }}
+                    >
+                        Email address
+                    </InputLabel>
+                    <Input
+                        id="my-input"
+                        classes={{
+                            root: 'contact__form__input'
+                        }}
+                    />
+                </div>
+            </FormControl>
+        </div>
+    );
+};
