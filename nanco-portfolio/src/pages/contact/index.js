24510e2e9ead49db1031850a6910706ef8dc1e6b
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-15 23:48:52 +0200

    #Contact Form
    Test form on netlify

--- nanco-portfolio/src/pages/contact/index.js
+++ nanco-portfolio/src/pages/contact/index.js
@@ -1,15 +1,45 @@
 import React from "react";
-import {FormControl, InputLabel, FilledInput} from '@material-ui/core';
+import {Button, FilledInput, FormControl, Icon, InputLabel} from '@material-ui/core';
+import {makeStyles} from "@material-ui/core/styles";
+import SendIcon from '@material-ui/icons/Send';
 import './contact.css';
 
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+    },
+}));
+
 export default () => {
+    const classes = useStyles();
     return (
         <div className="page-container contact">
             <p className="contact__title">Let's get in touch!</p>
-            <form className="contact__form">
+            <form
+                className="contact__form"
+                data-netlify="true"
+                name="contact-form"
+            >
+                <input type="hidden" name="form-name" value="contact" />
                 <FormControl fullWidth required variant='filled'>
                     <InputLabel
-                        htmlFor="my-input"
+                        htmlFor="name"
+                        classes={{
+                            root: 'contact__form__label',
+                            focused: 'contact__form__label--focused'
+                        }}
+                    >
+                        Your name
+                    </InputLabel>
+                    <FilledInput
+                        className='contact__form__input'
+                        id="name"
+                        type="text"
+                    />
+                </FormControl>
+                <FormControl fullWidth required variant='filled'>
+                    <InputLabel
+                        htmlFor="email"
                         classes={{
                             root: 'contact__form__label',
                             focused: 'contact__form__label--focused'
@@ -18,10 +48,38 @@ export default () => {
                         Email address
                     </InputLabel>
                     <FilledInput
-                        id="my-input"
                         className='contact__form__input'
+                        id="email"
+                        type="email"
                     />
                 </FormControl>
+                <FormControl fullWidth required variant='filled'>
+                    <InputLabel
+                        htmlFor="message"
+                        classes={{
+                            root: 'contact__form__label',
+                            focused: 'contact__form__label--focused'
+                        }}
+                    >
+                        Message
+                    </InputLabel>
+                    <FilledInput
+                        className='contact__form__input'
+                        id="message"
+                        type="text"
+                        multiline="true"
+                        rows={10}
+                    />
+                </FormControl>
+                <Button
+                    color="primary"
+                    className={classes.button}
+                    endIcon={<SendIcon/>}
+                    type='submit'
+                    variant="outlined"
+                >
+                    Send
+                </Button>
             </form>
         </div>
     );
