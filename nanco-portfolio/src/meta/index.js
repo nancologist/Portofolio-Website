1e861fc87fef2860fc22d4dc338a533d9d404793
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-17 16:13:19 +0200

    Add Android Chrome High-Res Favicon

--- nanco-portfolio/src/meta/index.js
+++ nanco-portfolio/src/meta/index.js
@@ -4,7 +4,10 @@ import {Helmet} from "react-helmet";
 const Meta = () => (
     <Helmet>
         <meta charSet="utf-8" />
+        {/* Set primary color for the tab in Android Chrome Browser: */}
         <meta name="theme-color" content="#86c232" />
+        {/* Set a high resolution favicon for Android Chrome: */}
+        <link rel="icon" sizes="192x192" href="../../public/icons/icon-192x192.png"/>
         <title>MJF | Fullstack Developer</title>
         {/*FONTS: Montserrat, Montserrat Alternates, Convergence, Zilla Slab, Rubik*/}
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap" rel="stylesheet"/>
