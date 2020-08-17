cd00428e640374cd7685656ac345d7b2dea8434a
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-17 23:29:02 +0200

    # Responsive

--- nanco-portfolio/src/pages/about/index.js
+++ nanco-portfolio/src/pages/about/index.js
@@ -10,12 +10,12 @@ export default () => (
             <div className="about-pic-container">
                 <div className="about-pic"/>
             </div>
-            <div className="about-content">
-                <h2>Codes, bugs, errors, my second family.</h2>
-                <p>
+            <div className="bio__content__text">
+                <p className="bio__content__title">Codes, bugs, errors, my second family.</p>
+                <p className="bio__content__text">
                     In July 2019 I have changed my career from Physics to IT and started my apprenticeship at <a target="_blank" href="https://www.digitaspixelpark.com/">Digitas Pixelpark</a>. It was not an overnight decision. My bachelor thesis had a small programming part with Matlab. It was a great experience for me, because more than coding I discovered that I love challenging errors and bugs! The longer it takes to solve them, the more excited I am to solve it. Giving up is no option for me.
                 </p>
-                <p>
+                <p className="bio__content__text">
                     In the last two years I have learned and tried dozens of languages and frameworks: Java, Python, PHP, JavaScript, Spring, Django, Vue, React, NodeJS, Express etc. to find the right one for me. Surprisingly I loved them all. They are all some kind of artworks to me, with different aspects and features.
                 </p>
             </div>
