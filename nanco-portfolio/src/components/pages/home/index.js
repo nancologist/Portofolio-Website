65e666e45cc2bc97d65ead0aa71c06cf762c6dfa
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-18 22:47:22 +0200

    # i18n
    Outsoure home page

---
+++ nanco-portfolio/src/components/pages/home/index.js
@@ -0,0 +1,52 @@
+import React, {Fragment, useRef} from "react"
+import {Link} from "gatsby";
+
+import './home.css';
+
+const Home = (props) => {
+    const homeBg = useRef(null);
+    const lightUpBackground = () => {
+        /*
+        * Lighting Up Background as hover-effect does not
+        * perform well on some browsers in some operating
+        * systems. So I disable this hover-effect for them.
+        *
+        * Black List:
+        *   - Firefox on MacOS
+        *   - ADD MORE...
+        * */
+        const {userAgent, platform}= navigator;
+        if (platform === 'MacIntel') {
+            if (userAgent.includes('Firefox')) {
+                return;
+            }
+        }
+        homeBg.current.classList.add('light-up')
+    };
+    return (
+        <Fragment>
+            <div className="home">
+                <div className="home-content">
+                    <h1>Morteza Jalilifar</h1>
+                    <p>
+                        {props.greeting1}
+                    </p>
+                    <p>
+                        {props.greeting2}
+                    </p>
+                    <Link
+                        to={props.aboutMeRoute}
+                        onMouseOver={lightUpBackground}
+                        onMouseOut={() => {homeBg.current.classList.remove('light-up')}}
+                    >
+                        {props.aboutMeText}
+                    </Link>
+                </div>
+                <div ref={homeBg} className="home-bg">
+                </div>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Home
--- nanco-portfolio/src/pages/index.js
+++ nanco-portfolio/src/pages/index.js
@@ -1,52 +1,15 @@
-import React, {Fragment, useRef} from "react"
-import {Link} from "gatsby";
+import React from "react"
 
 import './home.css';
+import Home from "../components/pages/home";
 
-const Home = () => {
-    const homeBg = useRef(null);
-    const lightUpBackground = () => {
-        /*
-        * Lighting Up Background as hover-effect does not
-        * perform well on some browsers in some operating
-        * systems. So I disable this hover-effect for them.
-        *
-        * Black List:
-        *   - Firefox on MacOS
-        *   - ADD MORE...
-        * */
-        const {userAgent, platform}= navigator;
-        if (platform === 'MacIntel') {
-            if (userAgent.includes('Firefox')) {
-                return;
-            }
-        }
-        homeBg.current.classList.add('light-up')
-    };
-    return (
-        <Fragment>
-            <div className="home">
-                <div className="home-content">
-                    <h1>Morteza Jalilifar</h1>
-                    <p>
-                        Hi, I'm Mori! Welcome to my page.
-                    </p>
-                    <p>
-                        This is a brief introduction of mine, my projects and skills, fasten your seatbelt!
-                    </p>
-                    <Link
-                        to='/about'
-                        onMouseOver={lightUpBackground}
-                        onMouseOut={() => {homeBg.current.classList.remove('light-up')}}
-                    >
-                        About Me
-                    </Link>
-                </div>
-                <div ref={homeBg} className="home-bg">
-                </div>
-            </div>
-        </Fragment>
-    );
-};
+const HomeEN = () => (
+    <Home
+        greeting1={`Hi, I'm Mori! Welcome to my page.`}
+        greeting2={`This is a brief introduction of mine, my projects and skills, fasten your seatbelt!`}
+        aboutMeRoute='/about'
+        aboutMeText='About Me'
+    />
+);
 
-export default Home
+export default HomeEN
