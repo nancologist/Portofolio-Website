002872b05d5ed07a80997387962b652030c4005a
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-19 03:22:26 +0200

    Fix German!

--- nanco-portfolio/src/pages/de/index.js
+++ nanco-portfolio/src/pages/de/index.js
@@ -4,7 +4,7 @@ import Home from "../../components/pages/home";
 const HomeDE = () => (
     <Home
         greeting1={`Willkommen zu meiner Seite.`}
-        greeting2={`Das ist eine kurze Vorstellung von mir, meiner Projekte und Kompetenzen.`}
+        greeting2={`Das ist eine kurze Einführung in meine professionelle Karriere, Projekte und Kompetenzen.`}
         aboutMeRoute='/de/ueber-mich'
         aboutMeText='Über mich'
         lang='de'
