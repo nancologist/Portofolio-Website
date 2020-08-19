d591eb1af89536a162fe1730cc90063b83706fe2
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-08-19 04:01:33 +0200

    Edit content.

--- nanco-portfolio/src/pages/index.js
+++ nanco-portfolio/src/pages/index.js
@@ -3,7 +3,7 @@ import Home from "../components/pages/home";
 
 const HomeEN = () => (
     <Home
-        greeting1={`Hi, I'm Mori! Welcome to my page.`}
+        greeting1={`Welcome to my page.`}
         greeting2={`This is a brief introduction of mine, my projects and skills, fasten your seatbelt!`}
         aboutMeRoute='/about'
         aboutMeText='About Me'
