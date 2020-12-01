c3281735f4b48b72535bc103bf8915b90d9940b8
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2020-12-01 11:23:54 +0100

    Clean up.

--- nanco-portfolio/src/pages/about/index.js
+++ nanco-portfolio/src/pages/about/index.js
@@ -40,14 +40,35 @@ export default (props) => {
                     </p>
                 </div>
             </div>
+            <div className="section-skills">
+                <h1 className="section__title">Skills</h1>
+                <div className="skill__list">
+                    <div className="skill__list-item">
+                        <h2>Frontend</h2>
+                    </div>
+                    <div className="skill__list-item">
+                        <h2>Backend</h2>
+                        <h3>Others</h3>
+                    </div>
+                    <div className="skill__list-item">
+                        <h2>Database</h2>
+                    </div>
+                    <div className="skill__list-item">
+                        <h2>Other Languages</h2>
+                    </div>
+                    <div className="skill__list-item">
+                        <h2>Tools</h2>
+                    </div>
+                    <div className="skill__list-item">
+                        <h2>Others</h2>
+                    </div>
+                </div>
+            </div>
             <div className="section-projects">
                 <h1 className="section__title">Works</h1>
                 <div className="section-projects__items">
                     <Project/>
                 </div>
-            </div>
-            <div className="section-skills">
-                <h1 className="section-projects--title">Works</h1>
             </div>
             <Snackbar
                 anchorOrigin={{ vertical: 'bottom', horizontal: 'right'}}
