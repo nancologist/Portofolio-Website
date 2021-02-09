548a5372290c92afd9a7e1f10498044ee54a6b16
Heisenberg <nancologist@gmail.com>
2021-02-09 06:59:32 +0100

    #Skills: Fix error

--- nanco-portfolio/src/util/util.js
+++ nanco-portfolio/src/util/util.js
@@ -1,12 +1,15 @@
 export function isElementVisible(el) {
-    const rect     = el.getBoundingClientRect(),
+    if (!!el) {
+        const rect     = el.getBoundingClientRect(),
         vWidth   = window.innerWidth || document.documentElement.clientWidth,
         vHeight  = window.innerHeight || document.documentElement.clientHeight,
         efp      = function (x, y) { return document.elementFromPoint(x, y) };     
 
-    if (rect.right < 0 || rect.bottom < 0 
-            || rect.left > vWidth || rect.top > vHeight)
-        return false;
+        if (rect.right < 0 || rect.bottom < 0 
+                || rect.left > vWidth || rect.top > vHeight)
+            return false;
 
-    return el.contains(efp((rect.left + rect.right) / 2, (rect.bottom + rect.top) / 2 ))
+        return el.contains(efp((rect.left + rect.right) / 2, (rect.bottom + rect.top) / 2 ))
+    }
+    return false;
 }
