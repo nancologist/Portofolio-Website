9a7933125d7f2b4dcc813433b0672ff77354619b
Heisenberg <nancologist@gmail.com>
2021-02-11 14:34:03 +0100

    Add IE warning.

--- nanco-portfolio/src/pages/index.js
+++ nanco-portfolio/src/pages/index.js
@@ -1,14 +1,29 @@
 import React from "react"
 import Home from "../components/pages/home";
 
-const HomeEN = () => (
-    <Home
-        greeting1={`Welcome to my page. I am a fullstack developer from Berlin.`}
-        greeting2={`This is a brief introduction of mine, my projects and skills, fasten your seatbelt!`}
-        aboutMeRoute='/about'
-        aboutMeText='About Me'
-        lang='en'
-    />
-);
+const HomeEN = () => {
+    
+    const ua = window.navigator.userAgent;
+    const isIE = /MSIE|Trident/.test(ua);
+    if (isIE) {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>Error: IE internet browsers are not supported!</h1>
+                <p>This is a modern webpage using modern technologies. To check it out please come back with a modern browser like Firefox, Chrome or Microsoft Edge.</p>
+                <p>Thank you! :)</p>
+            </div>
+        );
+    }
+
+    return (
+        <Home
+            greeting1={`Welcome to my page. I am a fullstack developer from Berlin.`}
+            greeting2={`This is a brief introduction of mine, my projects and skills, fasten your seatbelt!`}
+            aboutMeRoute='/about'
+            aboutMeText='About Me'
+            lang='en'
+        />
+    );
+}
 
 export default HomeEN
